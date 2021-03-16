c11c09d21aad5fe6a0a5ef6830a65cff02837e73
Galina <galinaborissenko@gmail.com>
2021-03-16 13:11:13 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const API = 'AIzaSyBLCVZD7ttGVxMh6EEJAkDaBKG6NPpR5T4'
+const MAX_RESULTS = 10
+
+const btn = document.getElementById('btn')
+const searchInput = document.getElementById('search')
+const output = document.getElementById('output')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+})
